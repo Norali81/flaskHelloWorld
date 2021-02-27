65667bb83547f3bad68ce55c68af8cf77ce74904
Nora Limbourg <noralimbourg@google.com>
2021-02-27 11:35:03 +0100

    App makes call to cloud vision

--- application.py
+++ application.py
@@ -27,14 +27,34 @@ def image():
 
 @app.route("/image_result", methods=["POST"])
 def image_result():
+    # This is an object of werkzeug.datastructures.FileStorage
     uploaded_file = request.files['image']
     filename = uploaded_file.filename
     
 
     if uploaded_file.filename != '':
-        print('This is error output', file=sys.stderr)
-        print('This is standard output', file=sys.stdout)
-        uploaded_file.save(uploaded_file.filename)
+        uploaded_file.save('./files/' + uploaded_file.filename)
 
-    return render_template("image_result.html", filename=filename, file=uploaded_file )
+        # The name of the image file to annotate
+        file_path = os.path.abspath('./files/' + filename)
+        print(file_path, file=sys.stderr)
+
+        # Loads the image into memory
+        with io.open(file_path, 'rb') as image_file:
+            content = image_file.read()
+
+        image = vision.Image(content=content)
+
+        # Instantiates a client
+        client = vision.ImageAnnotatorClient()
+
+        # Performs label detection on the image file
+        response = client.label_detection(image=image)
+        labels = response.label_annotations
+
+        print('Labels:')
+        for label in labels:
+            print(label.description, file=sys.stdout)
+
+    return render_template("image_result.html", filename=filename, file=content, labels=labels )
 
